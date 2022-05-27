28a1dc2cfd06cb8358f7d6d4606b85b6e042d182
Kevin De jesus <sebadjkevin@gmail.com>
2022-05-26 21:05:09 -0300

    Ejercicio 2

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ if edad <18:
     print("Eres un crio")
 else:
     print("Bienvenido señor")
+
+
+
+#Ejercicio 2
+
+inicial=int(input('Valor de inicio: '))
+final= int(input('Valor de finalzacion: '))
+
+
+for number in range(inicial,final):
+    if number%2!=0:
+        print(number)
