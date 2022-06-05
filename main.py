c303f5ae7656cb0a5e7d8e1041da18e99c274eb5
Kevin De jesus <sebadjkevin@gmail.com>
2022-06-05 17:58:16 -0300

    leccion 6 ejercicio 1

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 
 # Ejercicio 1
+import math
+
+# coding=utf-8
+
 edad=int(input('Ingresa tu edad: '))
 if edad <18:
-    print("Eres un crio")
+    print('Eres un crio')
 else:
-    print("Bienvenido señor")
-
-
+    print('Bienvenido señor')
 
 #Ejercicio 2
 inicial=int(input('Valor de inicio: '))
@@ -21,3 +23,14 @@ for number in range(inicial,final):
 
 for number in range(100,1,-1):
     print(number)
+
+
+#Leccion 6 , Ejercicio 1
+
+def triangleArea(altura,base):
+   return (base*altura)/2
+def circleRadio(radio):
+    print("------------Circle area-------------")
+    return (radio*math.pi)**2
+
+circleRadio(20)
