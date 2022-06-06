3e6cbf3ff7e8459f4f4ebb775deb912b1d8bfe6d
Kevin De jesus <sebadjkevin@gmail.com>
2022-06-06 00:00:14 -0300

    modulos, ejercicio

---
+++ modulos_ejercicio/main.py
@@ -0,0 +1,5 @@
+from operations.operations import sumar,restar,div,multi
+print(sumar(2,2))
+print(div(200,4))
+print(restar(3,4))
+print(multi(3,9))
---
+++ modulos_ejercicio/operations/operations.py
@@ -0,0 +1,8 @@
+def sumar(numero1,numero2):
+    return numero1+numero2
+def restar(numero1,numero2):
+    return numero1-numero2
+def multi(numero1,numero2):
+    return numero1*numero2
+def div(divisor,cociente):
+    return divisor/cociente
