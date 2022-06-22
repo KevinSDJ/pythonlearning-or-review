e58f3dc2f4f86ae491ffb4f665ac756696e262d5
KevinSDJ <sebadjkevin@gmail.com>
2022-06-22 04:11:07 -0300

    methods_ejercice ejercicio 2

--- modulos_ejercicio/methods_ejercice/main.py
+++ modulos_ejercicio/methods_ejercice/main.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import functools
+
+#Ejercicio 1
 print("Ingresa una lista de paises")
 
 listcountries=set()
@@ -13,4 +16,28 @@ while start:
                               
     
 listcountries= sorted(list(listcountries),reverse=False)
+print('Lista de paises ingresados:´')
 print(listcountries)
+print('\n')
+
+#Ejercicio 2
+print('Ejercicio 2 \n')
+
+def filterAndsum(lista):
+    entrytype=type(lista) == type([])
+    if entrytype:
+        pass
+    else:
+        print('Debes pasar una lista como argumento')
+        return
+    checkcontent=all([type(element)==type(4) for element in lista])
+    if checkcontent:
+        pass
+    else:
+        print('La lista solo debe contener numeros')
+    lista= filter(lambda x: x%2!=0,lista)
+    sum= functools.reduce(lambda x,b: x+b ,lista)
+    print(sum)
+
+filterAndsum([2,3,43,1])
+
